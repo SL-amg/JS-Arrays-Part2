7f82508d255d815dd0b35359b741de08d56f9118
Abdullah Abbas <sl_amg@msn.com>
2024-11-03 11:19:37 +0300

    updated coda after grade of 85 git add .

--- arrays2.js
+++ arrays2.js
@@ -9,18 +9,19 @@
  * isArrayLengthOdd([1, 2, 3, 4]) -> flase
  */
 function isArrayLengthOdd(numbers) { // return numbers.length % 2 !== 0;
-if (numbers.length%2 === 0){
-  return false;
-}
-else{
-  return true;
-}
+  if (numbers.length % 2 === 0) {
+    return false;
   }
-  
-const arraysample1 = [1, 2, 3];
-console.log(isArrayLengthOdd(arraysample1));
-const arraysample2 = [1, 2, 3, 4];
-console.log(isArrayLengthOdd(arraysample2));
+  else {
+    return true;
+  }
+}
+
+const arraySample1 = [1, 2, 3];
+console.log(isArrayLengthOdd(arraySample1));
+
+const arraySample2 = [1, 2, 3, 4];
+console.log(isArrayLengthOdd(arraySample2));
 
 /**
  * isArrayLengthEven(numbers):
@@ -33,55 +34,58 @@ console.log(isArrayLengthOdd(arraysample2));
  * isArrayLengthEven([1, 2, 3, 4]) -> true
  */
 function isArrayLengthEven(numbers) {
-    if (numbers.length%2 === 0){
-        return true;
-      }
-      else{
-        return false;
-      }
-        }
-        
-      const arraysample3 = [1, 2, 3];
-      console.log(isArrayLengthEven(arraysample3));
-      const arraysample4 = [1, 2, 3, 4];
-      console.log(isArrayLengthEven(arraysample4));
+  if (numbers.length % 2 === 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
-      /**
- * addLailaToArray(instructors):
- * - receives array `instructors`
- * - returns a new array that's a copy of array `instructors` with additional string "Laila"
- *
- * e.g.
- * addLailaToArray(["Mshary", "Hasan"]) -> ["Mshary", "Hasan", "Laila"]
- */
+const arraySample3 = [1, 2, 3];
+console.log(isArrayLengthEven(arraySample3));
+
+const arraySample4 = [1, 2, 3, 4];
+console.log(isArrayLengthEven(arraySample4));
+
+/**
+* addLailaToArray(instructors):
+* - receives array `instructors`
+* - returns a new array that's a copy of array `instructors` with additional string "Laila"
+*
+* e.g.
+* addLailaToArray(["Mshary", "Hasan"]) -> ["Mshary", "Hasan", "Laila"]
+*/
 function addLailaToArray(instructors) {
-    const newarray = instructors; // make a new array
-    newarray.push("Laila"); // add name to new array
-    return newarray // return vale of new array call newarray 
-  }
-  const oldnames = ["Mshary", "Hasan"];
-  console.log(addLailaToArray(oldnames));
+  const newArray = instructors; // make a new array
+  newArray.push("Laila"); // add name to new array
+  return newArray // return vale of new array call newarray 
+}
+const oldNames = ["Mshary", "Hasan"];
+console.log(addLailaToArray(oldNames));
 
-  const oldnames2 = ["Mshary", "Hasan", "Ali", "jassem"];
-  console.log(addLailaToArray(oldnames2));
+const oldNames2 = ["Mshary", "Hasan", "Ali"];
+console.log(addLailaToArray(oldNames2));
 
-  /**
- * eliminateTeam(teams):
- * - receives array `teams`
- * - removes the last element from the array and return it
- *
- * e.g.
- * eliminateTeam(["Brazil", "Germany", "Italy"]) -> "Italy"
- */
+/**
+* eliminateTeam(teams):
+* - receives array `teams`
+* - removes the last element from the array and return it
+*
+* e.g.
+* eliminateTeam(["Brazil", "Germany", "Italy"]) -> "Italy"
+*/
 function eliminateTeam(teams) {
-    const lastone = teams.pop();
-    // return `${teams} and the eleminated team is ${lastone}`; to return the teams and the elminated team
-return teams
-  }
-const nameofteams = ["Brazil", "Germany", "Italy"];
-console.log(eliminateTeam(nameofteams));
-const nameofteams2 = ["Brazil", "Germany", "Italy", "England", "Spain"];
-console.log(eliminateTeam(nameofteams2));
+  const lastOne = teams.pop();
+  // return `${teams} and the eleminated team is ${lastone}`; to return the teams and the elminated team
+  // if you return teams, it will return remaining array with remaining teams
+  return lastOne
+}
+const nameOfTeams = ["Brazil", "Germany", "Italy"];
+console.log(eliminateTeam(nameOfTeams));
+const nameOfTeams2 = ["Brazil", "Germany", "Italy", "England", "Spain"];
+console.log(eliminateTeam(nameOfTeams2));
+
 
 /** ------------------------------------ challange 1 -------------------------------------------------- */
 
@@ -96,21 +100,24 @@ console.log(eliminateTeam(nameofteams2));
  * secondHalfOfArrayIfItIsEven(["apple", "orange", "banana", "kiwi", "blueberry"]) -> []
  */
 function secondHalfOfArrayIfItIsEven(fruits) {
-    if (fruits.length%2 === 0){
-        const evenarray = fruits.slice(0,((fruits.length/2)));
-        return evenarray;
-      }
-      else{
-        return [];
-      }
+  if (fruits.length % 2 === 0) {
+    // const evenArray = fruits.slice(0, ((fruits.length / 2))); to show first half
+    const evenArray = fruits.slice(((fruits.length / 2)), fruits.length);
+    return evenArray;
   }
+  else {
+    return [];
+  }
+}
 
-  const newlist = ["apple", "orange", "banana", "kiwi"];
-  console.log(secondHalfOfArrayIfItIsEven(newlist));
-  const newlist2 = ["apple", "orange", "banana", "kiwi", "Carrots"]; // to test odd numbers
-  console.log(secondHalfOfArrayIfItIsEven(newlist2));
-  const newlist3 = ["apple", "orange", "banana", "kiwi", "Carrots", "tomatoes"];
-  console.log(secondHalfOfArrayIfItIsEven(newlist3));
+const newList = ["apple", "orange", "banana", "kiwi"];
+console.log(secondHalfOfArrayIfItIsEven(newList));
+
+const newList2 = ["apple", "orange", "banana", "kiwi", "Carrots"]; // to test odd numbers
+console.log(secondHalfOfArrayIfItIsEven(newList2));
+
+const newList3 = ["apple", "orange", "banana", "kiwi", "Carrots", "tomatoes"];
+console.log(secondHalfOfArrayIfItIsEven(newList3));
 
 // another soltuion to be tested later
 //   function secondHalfOfArrayIfItIsEven(fruits) {
@@ -140,31 +147,63 @@ function secondHalfOfArrayIfItIsEven(fruits) {
  * - Use string method .slice()
  */
 
-// const text = "Hello, world!"; //Testing
-// console.log(text); //testing
-// console.log(text.slice(0, 5)); //Testing
-// console.log(text.index("o"));
 
 function youGottaCalmDown(shout) {
-  let wordneeded = shout;
-  let locationOfEx = wordneeded.indexOf("!"); // gives location of ! first one , if non it will set locationofex as -1
-  while (locationOfEx !== -1 && wordneeded.indexOf("!", locationOfEx + 1) !== -1) {
-
+  let wordNeeded = shout;
+  let locationOfEx = wordNeeded.indexOf("!"); // gives location of ! first one , if non it will set locationofex as -1
+  while (locationOfEx !== -1 && wordNeeded.indexOf("!", locationOfEx + 1) !== -1) {
     // locationOfEx !== -1 to check if there is an ! in the string, if yes go inside loop 
-// wordneeded.indexOf("!", locationOfEx + 1) to check the position of the next ! after pervious loction (locationofEx), if no it will resulit in -1 and leave the loop
-    console.log(`Found "!" at index ${locationOfEx}`); // to show location of ! in the word but not working for me 
-    wordneeded = wordneeded.slice(0, -1); // Remove the last character
+    // wordneeded.indexOf("!", locationOfEx + 1) to check the position of the next ! after pervious loction (locationofEx), if no it will resulit in -1 and leave the loop 
+    wordNeeded = wordNeeded.slice(0, -1); // Remove the last character
   }
 
-  return wordneeded;
+  return wordNeeded;
 }
 let firstWord = "HI!!!!!!!!!!"
 console.log(youGottaCalmDown(firstWord));
-let secondWord = "Taylor Schwift!!!!!!!!!!!" ;
+
+let secondWord = "Taylor Schwift!!!!!!!!!!!";
 console.log(youGottaCalmDown(secondWord));
-let thirdWord = "Hellooooo" ;
+
+let thirdWord = "Hellooooo";
 console.log(youGottaCalmDown(thirdWord));
+
 let testWord = "Abdullah Abbas !!"
 console.log(youGottaCalmDown(testWord));
-let testWord2 = "Abdullah Abbas !! Al Abbas" // want to test but it is removing all after !!
-console.log(youGottaCalmDown(testWord2)); // to make this work i have to use wordneeded = wordneeded.slice(locationOfEx, (locationOfEx+1)); but getting erros dont know why
+
+
+
+/** ------------------------------------ challange 2  from end removal -------------------------------------------------- */
+/**
+ * youGottaCalmDown(shout):
+ * - receives a string `shout`
+ * - returns the string `shout` with at most one exclamation mark (!) at the end.
+ *
+ * e.g.
+ * youGottaCalmDown("HI!!!!!!!!!!") -> "HI!"
+ * youGottaCalmDown("Taylor Schwift!!!!!!!!!!!") -> "Taylor Schwift!"
+ * youGottaCalmDown("Hellooooo") -> "Hellooooo"
+ *
+ * Hint:
+ * - Use number method .indexOf()
+ * - Use string method .slice()
+ */
+
+function youGottaCalmDown2(shout) {
+  let neededWord = shout;
+  let stringCount = neededWord.length;
+  let locationOfEx2 = neededWord.indexOf("!");
+
+  while (locationOfEx2 !== -1 && neededWord.indexOf("!", locationOfEx2 +1) !== -1) {
+    // locationOfEx !== -1 to check if there is an ! in the string, if yes go inside loop 
+    // wordneeded.indexOf("!", locationOfEx + 1) to check the position of the next ! after pervious loction (locationofEx), if no it will resulit in -1 and leave the loop 
+    neededWord = neededWord.slice(0, -1); // Remove the last character
+  }
+  return neededWord;
+}
+
+let firstWord2 = "Marhabaaa!!!!!"
+console.log(youGottaCalmDown2(firstWord2));
+
+let firstWord3 = "Marhabaaa!!!!! Kuwait !!!!"
+console.log(youGottaCalmDown2(firstWord3));
